39f64588d28842ad104a67bd6f99c0e961b6f38c
parker.schuh@gmail.com <parker.schuh@gmail.com@1a61fbbb-f964-6b90-6ca4-db125235d84a>
2011-09-24 03:49:52 +0000

    Checking in my basic text displaying system.

--- trunk/client/MainWindow.h
+++ trunk/client/MainWindow.h
@@ -1,11 +1,13 @@
 #ifndef _GTK_CAD_MAIN_WINDOW_H_
 #define _GTK_CAD_MAIN_WINDOW_H_
 #include <gtk/gtk.h>
+#include "TextView.h"
 
 class MainWindow{
 public:
 	MainWindow();
 private:
+	TextView *view;
 	GtkWidget *window;
 	GtkWidget *vbox;
 };
--- trunk/client/TextView.cpp
+++ trunk/client/TextView.cpp
@@ -1,14 +1,23 @@
 #include "TextView.h"
-#include <gtk/gtk.h>
+
+gboolean TextView::Expose(){
+	context = new FontContext(GDK_DRAWABLE(text_area->window));
+	context->TestDraw();
+}
+gboolean TextView::Configure(){
+
+	return TRUE;
+}
+
+
 
 TextView::TextView(){
 	text_area = gtk_drawing_area_new();
 	gtk_widget_set_events (text_area, GDK_EXPOSURE_MASK | GDK_BUTTON_PRESS_MASK |
 			GDK_BUTTON_RELEASE_MASK | GDK_KEY_PRESS_MASK | GDK_BUTTON_MOTION_MASK);
-
+	g_signal_connect(text_area, "configure-event", G_CALLBACK(ConfigureStatic), this);
+        g_signal_connect(text_area, "expose-event", G_CALLBACK(ExposeStatic), this);
 	gtk_widget_show(text_area);
-
-
 }
 
 
