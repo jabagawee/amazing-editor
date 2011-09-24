cb931963897c1f2bc13e754bee1a1f8f936a55ef
andrew@jabagawee.com <andrew@jabagawee.com@1a61fbbb-f964-6b90-6ca4-db125235d84a>
2011-09-24 14:09:54 +0000

    we need a commit once in a wihle

--- trunk/client-py/notepad.py
+++ trunk/client-py/notepad.py
@@ -3,6 +3,26 @@
 import gtk
 import gtk.glade
 from diff_match_patch import diff_match_patch
+import sys
+import socket
+from random import randint
+
+def n2b(n):
+    return bytearray((n / 2 ** 24, n % 2 ** 24 / 2 ** 16, n % 2 ** 16 / 2 ** 8, n % 2 ** 8))
+b = bytearray
+
+server = '10.10.65.150'
+port = 19999
+user = 'user' + str(randint(1, 1000))
+password = 'cookies'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((server, int(port)))
+
+msg = b('a') + n2b(len(user)) + b(user) + n2b(len(password)) + b(password)
+s.send(msg)
+print s.recv(1024)
+
 
 tree = gtk.glade.XML("notepad.glade")
 
@@ -72,20 +92,55 @@ class Ask(object):
         self.window.destroy()
     def destroy(self, widget):
         self.calc.asking = False
-        
 
 pad = Notepad()
 
-old_text = pad.text
+filename = 'test1.txt'
+ver_num = 13
+
+msg = b('r') + n2b(len(filename)) + b(filename) + n2b(ver_num)
+s.send(msg)
+pad.text.set_text(s.recv(102400)[5:])
+
+msg = b('e') + n2b(len(filename)) + b(filename) + n2b(ver_num) 
+s.send(msg)
+s.recv(1024)
+
+def get_goddamn_text():
+    return pad.text.get_text(pad.text.get_start_iter(), pad.text.get_end_iter())
+
+old_text = get_goddamn_text()
 
 diffy_matchy_patchy = diff_match_patch()
 
 def communicate_with_server(*args):
+    '''Fuck you Stephen'''
+    # patch the old text
     global old_text
-    patches = diffy_matchy_patchy.patch_make(old_text, pad.text)
-    old_text = pad.text
-    print diffy_matchy_patchy(patch_toText(patches))
+    new_text = get_goddamn_text()
+    patches = diffy_matchy_patchy.patch_make(old_text, new_text)
+    old_text = get_goddamn_text()
+    patch_text = diffy_matchy_patchy.patch_toText(patches)
+    print patch_text
+
+    # send to server
+    msg = b('d') + n2b(len(patch_text)) + b(patch_text)
+    s.send(msg)
+    s.recv(1024)
+
+    # receive new shit
+    msg = b('g') + n2b(0)
+    s.send(msg)
+    response = s.recv(1024)
+
+    # apply patches to server
+    if response[0] == 'd':
+        patches = diffy_matchy_patchy.patch_fromText(response[5:])
+        pad.text.set_text(diffy_matchy_patchy.patch_apply(patches, get_goddamn_text())[0])
+
+    # done
     return True
+
 
 gtk.timeout_add(1000, communicate_with_server) # every second
 
