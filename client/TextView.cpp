b9eef6ad9cb44bc8d9c564387b918c2e94487527
andrew@jabagawee.com <andrew@jabagawee.com@1a61fbbb-f964-6b90-6ca4-db125235d84a>
2011-09-24 04:34:36 +0000

    committing a change from draw_area to text_view

--- client/TextView.cpp
+++ client/TextView.cpp
@@ -12,9 +12,12 @@ gboolean TextView::Configure(){
 
 
 TextView::TextView(){
-	text_area = gtk_drawing_area_new();
-	gtk_widget_set_events (text_area, GDK_EXPOSURE_MASK | GDK_BUTTON_PRESS_MASK |
-			GDK_BUTTON_RELEASE_MASK | GDK_KEY_PRESS_MASK | GDK_BUTTON_MOTION_MASK);
+	text_area = gtk_text_view_new();
+	gtk_widget_set_events (text_area, GDK_EXPOSURE_MASK |
+			                  GDK_BUTTON_PRESS_MASK |
+			                  GDK_BUTTON_RELEASE_MASK |
+					  GDK_KEY_PRESS_MASK |
+					  GDK_KEY_RELEASE_MASK);
 	g_signal_connect(text_area, "configure-event", G_CALLBACK(ConfigureStatic), this);
         g_signal_connect(text_area, "expose-event", G_CALLBACK(ExposeStatic), this);
 	gtk_widget_show(text_area);
